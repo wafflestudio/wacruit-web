56fdbfd10e7de4f6b5ad298016eedc3fda58d8e9
designdefined.by@gmail.com <designdefined.by@gmail.com>
2023-06-26 11:46:57 +0900

    Feat: setup msw

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import initMocks from "./mocks/index";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+if (process.env.NODE_ENV === "development") {
+  await initMocks();
+}
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
