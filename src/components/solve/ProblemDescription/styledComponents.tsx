de966955211497e6ef29dcecc4b7053c2828da63
isuk <ihyungsuk@snu.ac.kr>
2023-07-16 23:44:33 +0900

    FIX : spacer.ts 파일 이동에 따른 import 수정

--- src/components/solve/ProblemDescription/styledComponents.tsx
+++ src/components/solve/ProblemDescription/styledComponents.tsx
@@ -1,5 +1,5 @@
 import { styled } from "styled-components";
-import { spacer, SpacerProps } from "./spacer";
+import { spacer, SpacerProps } from "../../../lib/spacer";
 
 export const Text = styled.p<SpacerProps>`
   ${spacer}
