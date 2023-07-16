196882dc5fa141d5e47372242c0b0c602cf56da9
isuk <ihyungsuk@snu.ac.kr>
2023-07-16 20:56:37 +0900

    UPDATE : global styles 추가; styled-reset, styled-component 이용

---
+++ src/GlobalStyles.tsx
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyles = createGlobalStyle`
+  ${reset}
+  body {
+    * {
+      font-family: Pretendard, sans-serif;
+      box-sizing: border-box;
+      list-style-type: none;
+      text-decoration: none;
+      border-collapse: collapse;
+    }
+  }
+  a {
+      text-decoration: none;
+      color: inherit;
+    }
+  `;
+// ${{ ...reset, "font-family": "Pretendard" }}
+
+export default GlobalStyles;
