fc46b36185c7e19f6bd9788d1bc2073a3cb78b8c
designDefined <designdefined.by@gmail.com>
2023-07-26 03:37:45 +0900

    feat: recruiting mock api

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,5 +1,10 @@
 import { RestHandler } from "msw";
 import { sampleHandler } from "./handler/sampleHandler";
 import { userHandler } from "./handler/userHandler";
+import { recruitingHandler } from "./handler/recruitingHandler";
 
-export const handlers: RestHandler[] = [...sampleHandler, ...userHandler];
+export const handlers: RestHandler[] = [
+  ...sampleHandler,
+  ...userHandler,
+  ...recruitingHandler,
+];
---
+++ src/types/apiTypes.ts
@@ -0,0 +1,17 @@
+export type Recruiting = {
+  id: number;
+  name: string;
+  is_active: boolean;
+  from_date: string;
+  description: string;
+  problems: {
+    id: number;
+    num: number;
+    status: "미제출" | "채점중" | "성공" | "실패";
+  }[];
+};
+
+export type RecruitingItem = Pick<
+  Recruiting,
+  "id" | "name" | "is_active" | "from_date"
+> & { applicant_count: number };
