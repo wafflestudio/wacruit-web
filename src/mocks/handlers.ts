2feadfec48930d7a2993109a935b3d89aa5b1251
designDefined <designdefined.by@gmail.com>
2023-07-26 03:37:45 +0900

    feat: recruiting mock api

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,9 +2,11 @@ import { RestHandler } from "msw";
 import { sampleHandler } from "./handler/sampleHandler";
 import { userHandler } from "./handler/userHandler";
 import { HomeHandler } from "./handler/homeHandler";
+import { recruitingHandler } from "./handler/recruitingHandler";
 
 export const handlers: RestHandler[] = [
   ...sampleHandler,
-  ...userHandler,
   ...HomeHandler,
+  ...userHandler,
+  ...recruitingHandler,
 ];
