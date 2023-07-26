1cd1b01d41fc4bb1f182ba2bc79bb7c239fd95aa
designDefined <designdefined.by@gmail.com>
2023-07-26 10:49:51 +0900

    Feat: problem mock api handler

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,9 +2,11 @@ import { RestHandler } from "msw";
 import { sampleHandler } from "./handler/sampleHandler";
 import { userHandler } from "./handler/userHandler";
 import { recruitingHandler } from "./handler/recruitingHandler";
+import { problemHandler } from "./handler/problemHandler";
 
 export const handlers: RestHandler[] = [
   ...sampleHandler,
   ...userHandler,
   ...recruitingHandler,
+  ...problemHandler,
 ];
