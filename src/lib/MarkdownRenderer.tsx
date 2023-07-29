11bdc67d4830559e59704db90e54333553393490
isuk <ihyungsuk@snu.ac.kr>
2023-07-29 17:43:12 +0900

    UPDATE : styledComponent props 삭제 및 rehye-raw 패키지 적용

--- src/lib/MarkdownRenderer.tsx
+++ src/lib/MarkdownRenderer.tsx
@@ -1,24 +1,17 @@
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import remarkGfm from "remark-gfm";
-import { IStyledComponent } from "styled-components";
+import rehypeRaw from "rehype-raw";
 
 type MarkDownRendererProps = {
-  StyledComponent?: IStyledComponent<"web", string>;
   markdownString: string;
 };
 
 export default function MarkDownRenderer({
-  StyledComponent,
   markdownString,
 }: MarkDownRendererProps) {
-  if (StyledComponent !== undefined)
-    return (
-      <StyledComponent>
-        <ReactMarkdown children={markdownString} remarkPlugins={[remarkGfm]} />
-      </StyledComponent>
-    );
-  else
-    return (
-      <ReactMarkdown children={markdownString} remarkPlugins={[remarkGfm]} />
-    );
+  return (
+    <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
+      {markdownString}
+    </ReactMarkdown>
+  );
 }
