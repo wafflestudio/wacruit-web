c6f0687bc9633fc952ec0537e163f9a6f1a09f10
designDefined <designdefined.by@gmail.com>
2023-08-05 17:04:45 +0900

    Feat: delete file button

--- src/apis/portfolio.ts
+++ src/apis/portfolio.ts
@@ -17,10 +17,12 @@ export const postPortfolioFile = (fileName: string) =>
   getRequest<{ presigned_url: string; fields: object }>(
     `/portfolios/file/url/upload/?file_name=${fileName}`,
   );
-export const getPortfolioFile = (fileName: string) =>
-  getRequest<{ presigned_url: string; fields: object }>(
-    `/portfolios/file/url/download/?file_name=${fileName}`,
-  );
+export const downloadPortfolioFile = (fileName: string) =>
+  getRequest<{
+    object_name: string;
+    presigned_url: string;
+    fields: object | null;
+  }>(`/portfolios/file/url/download/?file_name=${fileName}`);
 export const deletePortfolioFile = (fileName: string) =>
   deleteRequest(`/portfolios/file/delete/?file_name=${fileName}`, {});
 
