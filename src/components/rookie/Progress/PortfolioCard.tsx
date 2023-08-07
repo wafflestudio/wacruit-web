5c14f3bc3bd1a4e780da3032802ebd3290c1fab3
designDefined <designdefined.by@gmail.com>
2023-08-08 00:17:12 +0900

    fix: portfolio url api 사용 방식 변경

--- src/components/rookie/Progress/PortfolioCard.tsx
+++ src/components/rookie/Progress/PortfolioCard.tsx
@@ -10,6 +10,7 @@ import {
   getPortfolioLinks,
   postPortfolioFile,
   postPortfolioLink,
+  putPortfolioLink,
   uploadPortfolioFileToS3,
 } from "../../../apis/portfolio";
 import { LoadingBackgroundBlink } from "../../../lib/loading";
@@ -158,6 +159,7 @@ export default function PortfolioCard() {
         <div>링크 첨부</div>
         {linksInput.map((input, index) => (
           <LinkInput
+            key={index}
             placeholder="https://example.com"
             value={input.url}
             onChange={(e) => {
@@ -166,14 +168,20 @@ export default function PortfolioCard() {
               setLinksInput(copy);
             }}
             onBlur={() => {
-              if (input.url.length < 1) return;
+              if (input.url.length < 1) {
+                if (input.id === null) return;
+                deletePortfolioLink(input.id).finally(() => {
+                  void queryClient.refetchQueries(["portfolio", "links"]);
+                });
+                return;
+              }
+
               if (input.id === null) {
                 postPortfolioLink(input.url).finally(() => {
                   void queryClient.refetchQueries(["portfolio", "links"]);
                 });
               } else {
-                deletePortfolioLink(input.id)
-                  .then(() => postPortfolioLink(input.url))
+                putPortfolioLink(input.id, input.url)
                   .catch((e) => console.log(e))
                   .finally(() => {
                     void queryClient.refetchQueries(["portfolio", "links"]);
