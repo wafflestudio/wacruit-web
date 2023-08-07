425d16730e1910eb9016558b61e15d6323bd4f77
designDefined <designdefined.by@gmail.com>
2023-08-07 23:33:10 +0900

    Fix: delete trailing slash

--- src/apis/announcement.ts
+++ src/apis/announcement.ts
@@ -2,6 +2,6 @@ import { TAnnouncement } from "../types/apiTypes";
 import { getRequest } from "./utility";
 
 export const getAllAnnouncements = () =>
-  getRequest<{ items: TAnnouncement[] }>("/announcements/", {}, false).then(
+  getRequest<{ items: TAnnouncement[] }>("/announcements", {}, false).then(
     (res) => res.items.reverse(),
   );
