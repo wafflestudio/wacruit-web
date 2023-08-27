068cf9b0ec2ed680c55b2a131d41ae9862e4ccfc
designDefined <designdefined.by@gmail.com>
2023-08-27 18:42:24 +0900

    Fix: remove initial delay

--- src/lib/animatedTransition/functions/delay.ts
+++ src/lib/animatedTransition/functions/delay.ts
@@ -1,12 +1,23 @@
+let isInitialDelay = true;
 let lastTimeoutId: null | number = null;
+
 export const setDelay = (ms: number, value: unknown = null): Promise<true> =>
   new Promise((resolve) => {
+    // clear previous timeout
     if (lastTimeoutId !== null) clearTimeout(lastTimeoutId);
+
+    // set delay to 0 if it's initial
+    const delay = isInitialDelay ? 0 : ms;
+    if (isInitialDelay) {
+      isInitialDelay = false;
+    }
+
+    // set new timeout
     lastTimeoutId = setTimeout(
       () => {
         resolve(true);
       },
-      ms,
+      delay,
       value,
     );
   });
