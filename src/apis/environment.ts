c6eb523d5e8da32f3ae1a3b62653e0f3f1225fd9
Minkyu Lee <whizkyu@snu.ac.kr>
2023-12-28 02:39:32 +0900

    sso redirect url 로컬에서도 작동하도록 수정

--- src/apis/environment.ts
+++ src/apis/environment.ts
@@ -6,4 +6,4 @@ export const SSO_LOGIN_URL =
     ? ""
     : import.meta.env.VITE_SSO_LOGIN_URL;
 
-export const SSO_REDIRECT_URL = import.meta.env.VITE_SSO_REDIRECT_URL;
+export const SSO_REDIRECT_URL = `${window.location.origin}/sso`;
