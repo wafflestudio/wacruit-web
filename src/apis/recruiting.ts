3a964721c05ed39301cc23cdd6de6664a40c14f0
Minkyu Lee <whizkyu@snu.ac.kr>
2024-04-15 03:25:56 +0900

    리크루팅 지원/포기 api 추가

--- src/apis/recruiting.ts
+++ src/apis/recruiting.ts
@@ -3,7 +3,7 @@ import {
   RecruitingResult,
   RecruitingSummary,
 } from "../types/apiTypes";
-import { getRequest } from "./utility";
+import { getRequest, postRequest, deleteRequest } from "./utility";
 
 export const getAllRecruitings = () =>
   getRequest<{ items: RecruitingSummary[] }>(`/v1/recruitings`, {}, false);
@@ -13,3 +13,9 @@ export const getRecruitingById = (id: Recruiting["id"]) =>
 
 export const getRecruitingResult = (id: Recruiting["id"]) =>
   getRequest<RecruitingResult>(`/v1/recruitings/${id}/result`);
+
+export const applyRecruiting = (id: Recruiting["id"]) =>
+  postRequest(`/v1/recruitings/${id}/apply`, {});
+
+export const cancelRecruiting = (id: Recruiting["id"]) =>
+  deleteRequest(`/v1/recruitings/${id}/apply`, {});
