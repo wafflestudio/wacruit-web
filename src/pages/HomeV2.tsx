7bf30846571ce70a9152a6a81531fb3698fb0ced
Yeonu-Kim <ywk0524@snu.ac.kr>
2025-07-03 22:07:35 +0900

    💄 숫자로 보는 역사 부분 ui 생성

--- src/pages/HomeV2.tsx
+++ src/pages/HomeV2.tsx
@@ -1,6 +1,7 @@
 import Headerv2 from "../components/home/Header/HeaderV2";
 import styled from "styled-components";
 import { HeroSection } from "../features/home/ui/HeroSection";
+import { WaffleHistory } from "../features/home/ui/WaffleHistory";
 
 export default function HomeV2() {
   return (
@@ -8,6 +9,7 @@ export default function HomeV2() {
       <Headerv2 />
       <MainContainer>
         <HeroSection />
+        <WaffleHistory />
       </MainContainer>
     </div>
   );
